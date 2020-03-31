3990fdffc2b13c2ec2132ae09ab5e5329df70f5e
unknown <semeschenok@vt-ros-008-us.vitebsk.beltelecom.by>
2020-03-31 12:19:39 +0300

    UNIT 2. Основы ввода данных, работа с input

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 console.log("hello,word!!");
+let a = 55;
+let b = "cxx_BY";
+console.log(a + ' ' + b);
 // document.getElementById('out').innerHTML = 'cxx';
 let one = document.querySelector('#one');
-let input = document.querySelector('#input');
-const clickIn = () => {
+let inputIn = document.querySelector('.inputIn');
+let button = document.querySelector('button');
 
+button.onclick = () => {
+    console.log('WORK!!!');
+    let b = inputIn.value;
+    console.log(b);
+    inputIn.value = '';
 }
