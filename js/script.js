cf25a703e3e995a95241043524647c28db37e685
unknown <semeschenok@vt-ros-008-us.vitebsk.beltelecom.by>
2020-03-31 12:28:16 +0300

    задачи к 2 уроку

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 console.log("hello,word!!");
-let a = 55;
-let b = "cxx_BY";
-console.log(a + ' ' + b);
+//let a = 55;
+//let b = "cxx_BY";
+//console.log(a + ' ' + b);
 // document.getElementById('out').innerHTML = 'cxx';
 let one = document.querySelector('#one');
 let inputIn = document.querySelector('.inputIn');
@@ -13,3 +13,21 @@ button.onclick = () => {
     console.log(b);
     inputIn.value = '';
 }
+
+//// Задача 1 /////
+
+let a = 7;
+let b = 9;
+console.log(a * b);
+
+//// Задача 2 /////
+
+let c = 7;
+let d = 9;
+console.log(c / d);
+
+//// Задача 3 /////
+
+let e = 3;
+let f = 5;
+console.log(e + f);
