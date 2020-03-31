89bfa5000c457219308f7c9de19650d57a77c43d
unknown <semeschenok@vt-ros-008-us.vitebsk.beltelecom.by>
2020-03-31 11:22:40 +0300

    UNIT 1. СТАРТУЕМ И ПИШЕМ ПЕРВУЮ ПРОГРАММУ

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log("hello,word!!");
+// document.getElementById('out').innerHTML = 'cxx';
+let one = document.querySelector('#one');
+let input = document.querySelector('#input');
+const clickIn = () => {
+
+}
