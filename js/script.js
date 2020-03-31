90eaf76b1dbfa45cc1003f2fbe2f91957e054098
evgeny-cxx <semeschenok@vt-ros-008-us.vitebsk.beltelecom.by>
2020-03-31 16:25:31 +0300

    UNIT 3. Ветвление If, else, switch

--- js/script.js
+++ js/script.js
@@ -1,33 +1,21 @@
-console.log("hello,word!!");
-//let a = 55;
-//let b = "cxx_BY";
-//console.log(a + ' ' + b);
-// document.getElementById('out').innerHTML = 'cxx';
-let one = document.querySelector('#one');
-let inputIn = document.querySelector('.inputIn');
-let button = document.querySelector('button');
+// let a = -16;
+
+// if (a > 9) {
+//     console.log('Yes!');
+// } else {
+//     console.log('else');
+// }
+
+const button = document.querySelector('button');
+const input = document.querySelector('.age');
+
+/* button.onclick = () => {
+    input.value >= 18 ? console.log("ACESS") : console.log('NO EXIT');
+    input.value = '';
+}; */
 
 button.onclick = () => {
-    console.log('WORK!!!');
-    let b = inputIn.value;
-    console.log(b);
-    inputIn.value = '';
+    switch (input.value) {
+        case 
+    }
 }
-
-//// Задача 1 /////
-
-let a = 7;
-let b = 9;
-console.log(a * b);
-
-//// Задача 2 /////
-
-let c = 7;
-let d = 9;
-console.log(c / d);
-
-//// Задача 3 /////
-
-let e = 3;
-let f = 5;
-console.log(e + f);
