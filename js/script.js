807a091fe96384614038e7525968992adaaec109
evgeny-cxx <semeschenok@vt-ros-008-us.vitebsk.beltelecom.by>
2020-04-01 15:04:47 +0300

    UNIT 5. ЦИКЛЫ В JAVASCRIPT (ЧАСТЬ 1)

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
-// let a = -16;
+/////// Задача 1 /////////
 
-// if (a > 9) {
-//     console.log('Yes!');
-// } else {
-//     console.log('else');
-// }
-
-const button = document.querySelector('button');
-const input = document.querySelector('.age');
-
-/* button.onclick = () => {
-    input.value >= 18 ? console.log("ACESS") : console.log('NO EXIT');
-    input.value = '';
-}; */
+const button = document.querySelector('.b-1');
+const output = document.getElementById('out-1');
 
 button.onclick = () => {
-    switch (input.value) {
-        case 
+    let out = '';
+    for (let i = 1; i <= 50; i++) {
+        out += i + ' ';
     }
-}
+    output.innerHTML = out;
+};
+
+/////// Задача 2 /////////
+
+const butt = document.querySelector('.b-2');
+const out = document.getElementById('out-2');
+
+butt.onclick = () => {
+    let out2 = '';
+    for (let i = 2; i <= 122; i += 2) {
+        out2 += i + ' ';
+    }
+    out.innerHTML = out2;
+};
+
+/////// Задача 3 /////////
+
+const but = document.querySelector('.b-3');
+const outt = document.getElementById('out-3');
+
+but.onclick = () => {
+    let out2 = '';
+    for (let i = 25; i >= 7; i--) {
+        out2 += i + ' ';
+    }
+    outt.innerHTML = out2;
+};
