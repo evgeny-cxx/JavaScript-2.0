196c28e0cb7cde0b2558a7c276a2e9c482b537e6
evgeny-cxx <evgeny-cxx@tut.by>
2020-04-03 17:19:27 +0300

    UNIT 8. ЦИКЛ WHILE, DO WHILE

--- js/script.js
+++ js/script.js
@@ -1,66 +1,74 @@
-const one = document.querySelector(".one");
+const out = document.querySelector(".out");
 
-one.style.width = "100px";
-one.style.paddingBottom = "80px";
+let p = 0;
+let flag = 3;
+let str = "";
 
-one.classList.add("two", "three");
-one.classList.remove("three");
-
-const toggle = document.querySelector(".toggle");
-
-toggle.onclick = function() {
-  this.classList.toggle("three");
-};
-const gas = document.querySelectorAll(".gas");
-for (i = 0; i < gas.length; i++) {
-  gas[i].onclick = function() {
-    let gallons = document.querySelector(".gallons").value;
-    let amount = this.getAttribute("data");
-    let out = document.querySelector(".out");
-    out.textContent = amount * gallons;
-  };
+while (p < 4) {
+  let p1 = 0;
+  while (p1 < 4) {
+    if (p1 < flag) {
+      str += "";
+    } else {
+      str += "*";
+    }
+    p1++;
+  }
+  flag--;
+  str += "<br>";
+  p++;
 }
 
-let a = document.createElement("div");
+out.innerHTML = str;
 
-a.innerHTML = "hello!";
-a.classList.add("one");
-a.onclick = function() {
-  alert("Hello");
-};
-
-document.querySelector(".test").appendChild(a);
-
-////// Задача 1 ////////
-
+///// Задача 1 ///////
+const out1 = document.querySelector(".out-1");
 const button = document.querySelector(".b-1");
-const out = document.querySelector(".out-1");
 
-function f1() {
-  out.style.width = "200px";
-  out.style.heigth = "40px";
+function t1() {
+  let x = 0;
+  let str = "";
+  while (x <= 50) {
+    str += x + " ";
+    x++;
+  }
+  console.log(str);
+  out1.innerHTML = str;
 }
 
-button.onclick = f1;
+button.onclick = t1;
 
-////// Задача 2 ////////
-
-const button2 = document.querySelector(".b-2");
+///// Задача 2 ///////
 const out2 = document.querySelector(".out-2");
+const button2 = document.querySelector(".b-2");
 
-function f2() {
-  out2.classList.add("bg-2");
+function t2() {
+  let x = 0;
+  let str = "";
+  while (x <= 122) {
+    str += x + " ";
+    x += 2;
+  }
+  console.log(str);
+  out2.innerHTML = str;
 }
 
-button2.onclick = f2;
+button2.onclick = t2;
 
-////// Задача 3 ////////
+///// Задача 3 ///////
 
-const button3 = document.querySelector(".b-3");
 const out3 = document.querySelector(".out-3");
+const button3 = document.querySelector(".b-3");
 
-function f3() {
-  out3.classList.remove("bg-3");
+function t3() {
+  let x = 25;
+  let str = "";
+  while (x >= 7) {
+    str += x + " ";
+    x--;
+  }
+  console.log(str);
+  out3.innerHTML = str;
 }
 
-button3.onclick = f3;
+button3.onclick = t3;
