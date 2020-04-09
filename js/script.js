7388fec407437677a52ebe2d03781e12142954be
evgeny-cxx <evgeny-cxx@tut.by>
2020-04-09 13:44:56 +0300

    UNIT 15. SET В JAVASCRIPT

--- js/script.js
+++ js/script.js
@@ -1,58 +1,59 @@
-const a = {
-  a: 5,
-  b: "hello",
-  z2: "Hi",
-};
-
-a.yy = 5555;
-a.b = 77;
-delete a.z2;
+let a = new Set();
+a.add(12);
+a.add(1);
+a.add("word");
+a.add(1);
+a.add("1"); //===
+// a.delete("word");
 console.log(a);
-
-let k = "z2";
-console.log(a[k]);
-
-let out = "";
-for (let key in a) {
-  out += `${key} -- ${a[key]} <br>`;
+console.log(a.length);
+console.log(a.size);
+console.log(a.has(1));
+console.log(a.has(5));
+for (let i of a) {
+  console.log(i);
 }
+let arr = [1, 2, 3, 4, 5, "hello", 5, 4, 3, 2, 1, 55, 5];
+let b = new Set(arr);
+console.log(b);
 
-document.querySelector(".out").innerHTML = out;
+/////// Задача 1 /////////
 
-////// Задача 1 ////////
-let a1 = {
-  one: 15,
-  two: 16,
-  five: 20,
+let s1 = new Set();
+s1.add("h");
+s1.add("b");
+s1.add("o");
+s1.add("h");
+console.log(s1);
+
+///// Задача 2 /////////
+const b1 = document.querySelector(".b-1");
+const in1 = document.querySelector(".i-1");
+const out1 = document.querySelector(".out-1");
+let s2 = new Set();
+
+const f2 = () => {
+  s2.add(in1.value);
+  console.log(s2);
+  return (out1.textContent = Array.from(s2));
 };
 
-function f1() {
-  let out = "";
-  for (let key in a1) {
-    if (key == "two") {
-      out += a1[key];
-    }
-  }
-  return (document.querySelector(".out-1").innerHTML = out);
+b1.onclick = f2;
+
+/////// Задача 3 /////////
+const b3 = document.querySelector(".b-3");
+const in3 = document.querySelector(".i-3");
+const out3 = document.querySelector(".out-3");
+let s3 = new Set([1, 2, 3, 4, 5, 6, 7]);
+// s3.delete(1);
+// console.log(s3);
+
+function f3() {
+  console.log(in3.value);
+  let i = in3.value;
+  s3.delete(+i);
+  console.log(s3);
+  return (out3.textContent = Array.from(s3));
 }
 
-document.querySelector(".b-1").onclick = f1;
-
-////// Задача 2 ////////
-let a2 = {
-  one: "hello",
-  two: "mahai",
-  five: "hi",
-};
-
-function f2() {
-  let out = "";
-  for (let key in a2) {
-    if (a2[key] == "hi") {
-      out += a2[key];
-    }
-  }
-  return (document.querySelector(".out-2").innerHTML = out);
-}
-
-document.querySelector(".b-2").onclick = f2;
+b3.onclick = f3;
