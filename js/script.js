dfadb22c4048180c76b8eb4e4fd25fe206ee127c
evgeny-cxx <evgeny-cxx@tut.by>
2020-04-10 14:16:29 +0300

    UNIT 20. СОБЫТИЯ КЛАВИАТУРЫ В JAVASCRIPT

--- js/script.js
+++ js/script.js
@@ -1,67 +1,56 @@
-document.querySelector(".one").onclick = (event) => {
-  console.log(event);
-  console.log("click");
-};
-document.querySelector(".two").ondblclick = () => {
-  console.log("double");
-};
-document.querySelector(".two").oncontextmenu = () => {
-  console.log("right button");
-  return false; // не будет контекстное меню
-};
+// // document.querySelector(".i-1").oninput = function (event) {
+// //   console.log(event);
+// // };
 
-let w = 75;
-document.querySelector(".three").onmousemove = () => {
-  document.querySelector(".three").style.width = w + "px";
-  w++;
-};
+// document.querySelector(".i-1").onkeypress = function (event) {
+//   console.log("keypress");
+//   console.log("charCode: " + event.charCode);
+//   console.log("key: " + event.key);
+//   console.log("keyCode: " + event.keyCode);
+//   console.log("code: " + event.code);
+//   // console.log(event);
+// };
 
-document.querySelector(".three").onmouseenter = () => {
-  document.querySelector(".three").style.background = "red";
-  console.log("1");
-};
+// document.querySelector(".i-1").onkeydown = function (event) {
+//   console.log("keydown");
+//   console.log("charCode: " + event.charCode);
+//   console.log("key: " + event.key);
+//   console.log("keyCode: " + event.keyCode);
+//   console.log("code: " + event.code);
+//   // console.log(event);
+// };
 
-document.querySelector(".three").onmouseleave = () => {
-  document.querySelector(".three").style.background = "green";
-  console.log("2");
-};
+// document.querySelector(".i-2").onkeypress = function (event) {
+//   console.log("charCode: " + event.charCode);
+//   console.log("key: " + event.key);
+//   console.log(event);
+//   const a = {
+//     q: "w",
+//     w: "e",
+//   };
+//   document.querySelector(".i-2").value = a[event.key];
+//   return false;
+// };
 
-document.querySelector(".three").onmousedown = () => {
-  document.querySelector(".three").style.background = "orange";
-  console.log("3");
-};
+/////// Задача 1////////////
 
-document.querySelector(".three").onmouseup = () => {
-  document.querySelector(".three").style.background = "blue";
-  console.log("4");
-};
-
-let p = 10;
-document.querySelector("button").onclick = () => {
-  p++;
-  document.querySelector("progress").value = p;
+document.querySelector(".i-1").onkeypress = f1 = (event) => {
+  return (document.querySelector(".out-1").textContent += event.key);
 };
 
-///// Задача 1 /////////
+/////// Задача 2////////////
 
-document.querySelector(".div-1").onclick = () => {
-  document.querySelector(".out-1").innerHTML = document.querySelector(
-    ".div-1"
-  ).innerHTML;
+document.querySelector(".i-2").onkeypress = f2 = (event) => {
+  return (document.querySelector(".out-2").textContent = event.keyCode + " , ");
 };
 
-///// Задача 2 /////////
+/////// Задача 3////////////
 
-document.querySelector(".div-2").ondblclick = (event) => {
-  console.log(event.altKey);
-  document.querySelector(".out-2").innerHTML = "key alt press: " + event.altKey;
-};
-
-///// Задача 3 /////////
-let wdt = 75;
-document.querySelector(".div-3").onclick = () => {
-  document.querySelector(".div-3").style.width = wdt + "px";
-  wdt += 5;
-  document.querySelector(".out-3").innerHTML = "ширина: " + wdt;
-  console.log(wdt);
+document.querySelector(".i-3").onkeypress = f3 = (event) => {
+  // console.log(event);
+  if (event.keyCode > 57) {
+    return (document.querySelector(".out-3").textContent = true);
+  } else {
+    return (document.querySelector(".out-3").textContent = false);
+  }
 };
