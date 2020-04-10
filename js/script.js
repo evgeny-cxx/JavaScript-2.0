67335cb0202d5ab51908dd71ed2bde9c2f67d269
evgeny-cxx <evgeny-cxx@tut.by>
2020-04-10 18:11:42 +0300

    UNIT 21. КРАТКИЙ ОБЗОР TOUCH СОБЫТИЙ

--- js/script.js
+++ js/script.js
@@ -1,56 +1,33 @@
-// // document.querySelector(".i-1").oninput = function (event) {
-// //   console.log(event);
-// // };
+/////// 1 ////////
+document.querySelector(".block-1").addEventListener("touchstart", handleStart);
 
-// document.querySelector(".i-1").onkeypress = function (event) {
-//   console.log("keypress");
-//   console.log("charCode: " + event.charCode);
-//   console.log("key: " + event.key);
-//   console.log("keyCode: " + event.keyCode);
-//   console.log("code: " + event.code);
-//   // console.log(event);
-// };
+function handleStart(event) {
+  console.log("+");
+  console.log(event);
+  document.querySelector(".out-1").textContent += "touch" + " ";
+}
 
-// document.querySelector(".i-1").onkeydown = function (event) {
-//   console.log("keydown");
-//   console.log("charCode: " + event.charCode);
-//   console.log("key: " + event.key);
-//   console.log("keyCode: " + event.keyCode);
-//   console.log("code: " + event.code);
-//   // console.log(event);
-// };
+/////// 2 ////////
+document.querySelector(".block-2").addEventListener("touchstart", handleStart2);
+let x = 0;
+function handleStart2(event) {
+  console.log("+");
+  console.log(event);
+  x += +event.touches.length;
+  console.log(x);
+  document.querySelector(".out-2").innerHTML = x;
+}
+/////// 3 ////////
+document
+  .querySelector(".block-3_1")
+  .addEventListener("touchstart", handleStart3);
 
-// document.querySelector(".i-2").onkeypress = function (event) {
-//   console.log("charCode: " + event.charCode);
-//   console.log("key: " + event.key);
-//   console.log(event);
-//   const a = {
-//     q: "w",
-//     w: "e",
-//   };
-//   document.querySelector(".i-2").value = a[event.key];
-//   return false;
-// };
+document
+  .querySelector(".block-3_2")
+  .addEventListener("touchstart", handleStart3);
 
-/////// Задача 1////////////
-
-document.querySelector(".i-1").onkeypress = f1 = (event) => {
-  return (document.querySelector(".out-1").textContent += event.key);
-};
-
-/////// Задача 2////////////
-
-document.querySelector(".i-2").onkeypress = f2 = (event) => {
-  return (document.querySelector(".out-2").textContent = event.keyCode + " , ");
-};
-
-/////// Задача 3////////////
-
-document.querySelector(".i-3").onkeypress = f3 = (event) => {
-  // console.log(event);
-  if (event.keyCode > 57) {
-    return (document.querySelector(".out-3").textContent = true);
-  } else {
-    return (document.querySelector(".out-3").textContent = false);
-  }
-};
+function handleStart3(event) {
+  console.log("+");
+  console.log(event);
+  document.querySelector(".out-3").textContent = event.target.outerText;
+}
