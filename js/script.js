623975597aa6f078d545272977f23ec462f83595
evgeny-cxx <evgeny-cxx@tut.by>
2020-04-14 21:07:35 +0300

    UNIT 28. ООП В ES6

--- js/script.js
+++ js/script.js
@@ -1,23 +1,9 @@
-// fetch("http://getpost.itgid.info/index2.php")
-//   .then((data) => data.text())
-//   .then((data) => {
-//     console.log(data);
-//   });
+let m = new Alert("hello", "alert-dark", ".out-1");
+console.log(m);
+m.showAlert();
 
-fetch("http://getpost.itgid.info/index2.php", {
-  method: "POST", // *GET, POST, PUT, DELETE, etc.
-  mode: "cors", // no-cors, *cors, same-origin
-  cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-  credentials: "same-origin", // include, *same-origin, omit
-  headers: {
-    "Content-Type": "application/json",
-    // 'Content-Type': 'application/x-www-form-urlencoded',
-  },
-  redirect: "follow", // manual, *follow, error
-  referrerPolicy: "no-referrer", // no-referrer, *client
-  body: "!!!aut_key_code&action=1", // body data type must match "Content-Type" header
-})
-  .then((response) => response.text())
-  .then((response) => {
-    console.log(response);
-  });
+let v = new Alert("good", "alert-info", ".out-2");
+v.showAlert();
+
+let i = new Alert2("pp", "alert-success", ".out-3", "pets");
+i.showIconAlert();
